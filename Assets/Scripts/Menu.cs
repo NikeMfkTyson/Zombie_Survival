96e5e83dff7883557e31261938d3e3b0c0c29eff
NikeMfkTyson <nikitazavadskiy@gmail.com>
2023-05-16 22:37:17 +0300

    Title-screen1
    added InputField and code
    save player name in MainManager

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class Menu : MonoBehaviour
 {
+    public GameObject titleScreen;
+    public GameObject buttons;
+    public InputField input;
+    public Text playerName;
     
     void Start()
     {
-        
+        buttons.SetActive(false);
+        titleScreen.SetActive(true);
+    }
+
+    public void SavePlayerName()
+    {        
+        MainManager.Instance.playerName = input.text;
+        playerName.text = "Player: " + MainManager.Instance.playerName;
+        buttons.SetActive(true);
+        titleScreen.SetActive(false);
+        print(MainManager.Instance.playerName);
     }
 
     public void Play()
